461353da68224a401f0868ca84dbceed7b31bb56
vaious02 <vaious02@gmail.com>
2026-09-22 23:24:17 +0700

    Update config.js
    LIFF ID

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@ export const SUPABASE_URL = 'https://cfmoqebpbzypkhplnqnr.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmbW9xZWJwYnp5cGtocGxucW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMjUzNTcsImV4cCI6MjEwMjgwMTM1N30.mWo1nM7Pyo4sQ-ALq6qDhu4wl9oa_yzHvDepCfIhhRA'; // Supabase > Project Settings > API > anon public
 
 // LIFF ID จาก LINE Developers > LINE Login channel > LIFF (ใช้เมื่อเปิดโหมด "ต้องล็อกอิน LINE" ในหน้าแอดมิน)
-export const LIFF_ID = '';
+export const LIFF_ID = '2011700777-REHXLSqL';
